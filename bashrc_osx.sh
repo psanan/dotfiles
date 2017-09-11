5acba84439bfe495b0dfd2e885d8e18c6ac90b95
Patrick Sanan <patrick.sanan@gmail.com>
2017-09-11 20:42:13 +0200

    OSX bashrc: magma ocean code stuff

--- bashrc_osx.sh
+++ bashrc_osx.sh
@@ -12,6 +12,13 @@ export PYTHONPATH=$PYTHONPATH:/Users/patrick/pythontestharness/lib
 export PDS_PETSC_ARCHNAME=darwin
 export PTATIN_DIR=$HOME/ptatin3d
 
+# For magma ocean code (corresponds to install hints there)
+alias setpdirectopt="export PETSC_DIR=$HOME/petsc-double-direct;export PETSC_ARCH=arch-darwin-c-opt;export PMPI=$HOME/petsc-double-direct/arch-darwin-c-opt/bin/mpiexec; whichp;"
+alias setpdirect="export PETSC_DIR=$HOME/petsc-double-direct;export PETSC_ARCH=arch-darwin-c-debug;export PMPI=$HOME/petsc-double-direct/arch-darwin-c-debug/bin/mpiexec; whichp;"
+alias setpdirectopt128="export PETSC_DIR=$HOME/petsc-quad-direct;export PETSC_ARCH=arch-darwin-c-opt;export PMPI=$HOME/petsc-quad-direct/arch-darwin-c-opt/bin/mpiexec; whichp;"
+alias setpdirect128opt=setpdirectopt128
+alias setpdirect128="export PETSC_DIR=$HOME/petsc-quad-direct;export PETSC_ARCH=arch-darwin-c-debug;export PMPI=$HOME/petsc-quad-direct/arch-darwin-c-debug/bin/mpiexec; whichp;"
+
 # Prompt, without hostname
 # ( Don't forget the \[ \], or you'll have line-wrapping issues )
 PS1='\[\e[1;31m\][\[\e[1;31m\]\W\[\e[0;36m\]$(__git_ps1 " (%s)")\[\e[1;31m\]]\$\[\e[0m\] '
@@ -30,7 +37,7 @@ function title {
 #  and to cd to the Downloads directory so attachments will end up there
 alias mutt="cd ~/Downloads;echo -e \"\\033]6;1;bg;blue;brightness;155\\a\";echo -e \"\\033]6;1;bg;red;brightness;55\\a\";echo -e \"\\033]6;1;bg;green;brightness;55\\a\";mutt;echo -e \"\\033]6;1;bg;*;default\\a\";cd -"
 
-# Set the tab title 
+# Set the tab title
 function title {
     echo -e "\033]6;1;bg;red;brightness;155\a";
     echo -e "\033]6;1;bg;green;brightness;55\a";
@@ -44,3 +51,6 @@ export IDEATRON_DATA_DIR=/Users/patrick/Google\ Drive/Ideatron_data
 
 source $HOME/.helpers.sh
 source $HOME/pdsnotes/helpers.sh
+
+# Magma ocean shorcut
+alias cdm="cd $HOME/magma-ocean/code/evolution/c/src_mpi"
