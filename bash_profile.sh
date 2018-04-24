d2b81c154ee273bd46e8057648d347d1e54dcf94
Patrick Sanan <patrick.sanan@gmail.com>
2018-04-24 08:15:33 +0200

    bash_profile: on login, print any running screens

--- bash_profile.sh
+++ bash_profile.sh
@@ -4,3 +4,11 @@ source $HOME/.bashrc
 # Display paths after manipulations by the other startup files
 printf "\033[34mPATH=\033[0m$PATH\n"
 printf "\033[34mPYTHONPATH=\033[0m$PYTHONPATH\n"
+
+# If GNU screen exists and there are any screens running, display them
+if command -v screen > /dev/null; then
+  printf "\033[34mGNU Screen:\033[0m "
+  if ! screen -ls; then
+    screen -ls
+  fi;
+fi;
