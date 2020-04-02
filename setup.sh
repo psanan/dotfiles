21212a27c408a3f3313aa8f2d105cd9dc5eb58ba
Patrick Sanan <patrick.sanan@gmail.com>
2020-04-02 09:22:12 +0200

    setup.sh: fix typo in setup for vim python indent settings

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ $LINK $PSANAN_RC_ROOT/bash_profile      $HOME/.bash_profile
 $LINK $PSANAN_RC_ROOT/bashrc            $HOME/.bashrc
 $LINK $PSANAN_RC_ROOT/vimrc             $HOME/.vimrc
 mkdir -p $HOME/.vim/after/indent
-$LINK $PSANAN_RC_ROOT/.vim/after/indent/python.vim $HOME/.vimrc/after/indent/python.vim
+$LINK $PSANAN_RC_ROOT/vim/after/indent/python.vim $HOME/.vimrc/after/indent/python.vim
 $LINK $PSANAN_RC_ROOT/latexmkrc         $HOME/.latexmkrc
 $LINK $PSANAN_RC_ROOT/gdbinit           $HOME/.gdbinit
 $LINK $PSANAN_RC_ROOT/octaverc          $HOME/.octaverc
