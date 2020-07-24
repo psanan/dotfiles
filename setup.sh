2e24e1807975127d134b2124adf0047c0b474ff4
Patrick Sanan <patrick.sanan@gmail.com>
2020-07-24 10:02:32 +0200

    Add YAPF config

--- setup.sh
+++ setup.sh
@@ -13,8 +13,11 @@ $LINK $PSANAN_RC_ROOT/latexmkrc    $HOME/.latexmkrc
 $LINK $PSANAN_RC_ROOT/octaverc     $HOME/.octaverc
 $LINK $PSANAN_RC_ROOT/screenrc     $HOME/.screenrc
 
-$LINK $PSANAN_RC_ROOT/vimrc        $HOME/.vimrc
 mkdir -p $HOME/.vim/after/indent
-$LINK $PSANAN_RC_ROOT/vim/after/indent/python.vim $HOME/.vim/after/indent/python.vim
 mkdir -p $HOME/.vim/spell
+$LINK $PSANAN_RC_ROOT/vimrc                       $HOME/.vimrc
+$LINK $PSANAN_RC_ROOT/vim/after/indent/python.vim $HOME/.vim/after/indent/python.vim
 $LINK $PSANAN_RC_ROOT/vim/spell/en.utf-8.add      $HOME/.vim/spell/en.utf-8.add
+
+mkdir -p $HOME/.config/yapf
+$LINK $PSANAN_RC_ROOT/config/yapf/style           $HOME/.config/yapf/style
